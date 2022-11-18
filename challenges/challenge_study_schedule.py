4efc316e50e046d75be4adc2c628b9c573f50a6c
diegocamomila <diego.camomila@gmail.com>
2022-11-18 19:46:06 -0300

    FEAT: Implementará a funçao study_schedule em challenges/challenge_study_schedule.py

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    counter_study = 0
+
+    for period in permanence_period:
+        if type(period[0]) != int or type(period[1]) != int:
+            return None
+
+        if target_time >= period[0] and target_time <= period[1]:
+            counter_study += 1
+
+    return counter_study
