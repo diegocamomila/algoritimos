dda0366f7b884a485f9482d1600e9bbcd31c54e8
diegocamomila <diego.camomila@gmail.com>
2022-11-18 19:49:53 -0300

    FIX: Implementará a funçao study_schedule em challenges/challenge_study_schedule.py

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,10 +3,10 @@ def study_schedule(permanence_period, target_time):
     counter_study = 0
 
     for period in permanence_period:
-        if type(period[0]) != int or type(period[1]) != int:
+        try:
+            if target_time >= period[0] and target_time <= period[1]:
+                counter_study += 1
+        except (ValueError, TypeError):
             return None
-
-        if target_time >= period[0] and target_time <= period[1]:
-            counter_study += 1
 
     return counter_study
